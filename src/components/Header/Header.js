1956fc8c645f1e3de018078de12f083693fbb8db
Hamza El Mkhantar <69247740+HamzaElMkhantar@users.noreply.github.com>
2023-04-03 20:30:07 +0000

    Add files via upload

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className='text-center pt-3 '>
       <h2>User Profile Details</h2>
-      <p>User profile details info using 'jsonplaceholder' API and implement React Router Functionality using 'react-router-dom' as well as bootstrap and react-bootstrap for UI</p>
+      <p>User profile details info using 'jsonplaceholder' API</p>
     </div>
   );
 };
